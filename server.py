891c8e724db761615d7c8d7a6ee34f28776adf1b
steinskeeper <dpelpsykongroo@gmail.com>
2022-12-09 18:23:00 +0530

    logic of url

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+from functools import partial
+from itertools import groupby
+from operator import itemgetter
+from happytransformer import HappyTextToText, TTSettings
+import re
+import whisper
 from flask import Flask
 import os
 from flask import Flask, flash, request, redirect, url_for, jsonify
@@ -7,20 +13,22 @@ import subprocess
 from pymongo import MongoClient
 import json
 import shortuuid
+import io
+from contextlib import redirect_stdout
+mysp = __import__("my-voice-analysis")
+
 
 # grammar model
-from happytransformer import HappyTextToText, TTSettings
 happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
 args = TTSettings(num_beams=5, min_length=1)
 
 # transcription -> audio to text
-import whisper
 whisperModel = whisper.load_model("base")
 
 
 app = Flask(__name__)
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {"webm" , "mp4"}
+ALLOWED_EXTENSIONS = {"webm", "mp4"}
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 CORS(app)
 
@@ -39,49 +47,147 @@ def allowed_file(filename):
 def home():
     return "Hello, World!"
 
-@app.route("/processvideo", methods = ["POST"])
+
+@app.route("/processvideo", methods=["POST"])
 def processVideo():
     if 'file' not in request.files:
-        return jsonify({"message" : "no video file found" , "code" : "error"})
+        return jsonify({"message": "no video file found", "code": "error"})
     file = request.files['file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'],"video", filename))  
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], "video", filename))
         # clip = VideoFileClip(file)
         # clip.audio.write_audiofile(f"uploads/audio/{filename}.mp3")
-        subprocess.call(["ffmpeg", "-y", "-i", "./uploads/video/"+filename, f"./uploads/audio/{filename}.mp3"], 
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.STDOUT)
-                    
+        subprocess.call(["ffmpeg", "-y", "-i", "./uploads/video/"+filename, f"./uploads/audio/{filename}.mp3"],
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.STDOUT)
+
         data = json.loads(request.form["data"])
         print(data)
         s = shortuuid.encode(u)
         short = s[:5]
+        result = whisperModel.transcribe("uploads\\audio\\" + filename)
         take.insert_one(
-            {"filename": filename, "script": data["script"], "scriptname": data["scriptname"], "practiceid": short})
+            {"filename": filename, "script": data["script"], "scriptname": data["scriptname"], "practiceid": short, "transcribed_script": result["text"]})
 
-        return jsonify({"message": "Saved" , "code": "success" })
+        return jsonify({"message": "Saved", "code": "success", "practiceid": short})
 
 
-@app.route("/correct-grammar", methods= ["POST"])
+@app.route("/correct-grammar", methods=["POST"])
 def correctGrammar():
     text = request.get_json()["text"]
     print(text)
     res = happy_tt.generate_text(text, args=args)
     print(res)
-    return jsonify({ "corrected" : res.text })
+    return jsonify({"corrected": res.text})
 
 
-@app.route("/transcribe", methods= ["POST"])
+@app.route("/transcribe", methods=["POST"])
 def transcribe():
     filename = request.get_json()["filename"]
     # print(text)
 
-    result  = whisperModel.transcribe("uploads\\audio\\"+ filename)
-    f = open("uploads\\transcription\\" + filename + ".txt" , "x")
+    result = whisperModel.transcribe("uploads\\audio\\" + filename)
+    f = open("uploads\\transcription\\" + filename + ".txt", "x")
     f.write(result["text"])
     f.close()
-    return jsonify({ "transcribe" : result["text"] })
+    return jsonify({"transcribe": result["text"]})
+
+
+@app.route("/get-take", methods=["POST"])
+def getTake():
+    data = request.get_json()
+    print(data)
+    result = take.find_one({"practiceid": data["practiceid"]})
+    sn = result["scriptname"]
+    pastdata = {
+        "wpm": [],
+        "pauses": [],
+        "speechpercent": [],
+    }
+    prev = take.find({"scriptname": sn})
+    for p in prev:
+        pastdata["wpm"].append(p["analytics"]["wpm"])
+        pastdata["pauses"].append(p["analytics"]["pauses"])
+        pastdata["speechpercent"].append(p["analytics"]["speechpercent"])
+    final = {
+        "scriptname": result["scriptname"],
+        "script": result["script"],
+        "transcribedScript": result["transcribed_script"],
+        "analytics": result["analytics"],
+        "pastdata": pastdata
+    }
+    return jsonify({"take": final})
+
+
+def del_ret(d, key):
+    del d[key]
+    return d
+
+
+@app.route(rule="/getalltakes", methods=["GET"])
+def getTakes():
+    takes = []
+    result = take.find({})
+    for r in result:
+        takes.append(r)
+
+    pop = dict(map(lambda k_v: (k_v[0], tuple(map(partial(del_ret, key="scriptname"), k_v[1]))),
+                   groupby(takes, itemgetter("scriptname"))))
+
+    return jsonify({"takes": pop})
+
+
+@app.route("/analysis", methods=["POST"])
+def analysis():
+    data = request.get_json()
+    practiceid = data["practiceid"]
+    result = take.find_one({"practiceid": practiceid})
+    file = result["filename"]
+    script = result["script"]
+    p = file
+    c = r"./uploads/audio"
+
+    sr = io.StringIO()
+    with redirect_stdout(sr):
+        mysp.myspsr(p, c)
+    speechrate = sr.getvalue()
+
+    numpause = io.StringIO()
+    with redirect_stdout(numpause):
+        mysp.mysppaus(p, c)
+    pauses = numpause.getvalue()
+    pauses = re.findall("\d+", pauses)
+    pauses = int(pauses[0])
+
+    speakdur = io.StringIO()
+    with redirect_stdout(speakdur):
+        mysp.myspst(p, c)
+    speakingduration = speakdur.getvalue()
+    speakingduration = re.findall("\d+\.\d+", speakingduration)
+    speakingduration = float(speakingduration[0])
+
+    orgdur = io.StringIO()
+    with redirect_stdout(orgdur):
+        mysp.myspod(p, c)
+    originalduration = orgdur.getvalue()
+    originalduration = re.findall("\d+\.\d+", originalduration)
+    originalduration = float(originalduration[0])
+
+    speechpercentage = (speakingduration/originalduration)*100
+
+    moodtemp = io.StringIO()
+    with redirect_stdout(moodtemp):
+        mysp.myspgend(p, c)
+    tone = ""
+    mood = moodtemp.getvalue()
+    if "Showing no emotion" in tone:
+        tone = "Emotionless"
+    elif "Reading" in tone:
+        tone = "Reading"
+    elif "passionately" in tone:
+        tone = "Passionate"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
